8a64c9deabd31102cc621ea8e915bb518eff4822
Gakuto Yamada <yamada_gakuto@cyberagent.co.jp>
2015-11-13 16:11:29 +0900

    no message

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/user', users);
 app.use('/movie', movie);
 app.use('/item', item);
 app.use('/post', post);
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var request = require('superagent');
+var async = require('async');
 var router = express.Router();
 
 /* GET users listing. */
@@ -6,9 +8,34 @@ router.get('/', function (req, res, next) {
     res.send('respond with a resource');
 });
 
-router.get('/:id(\\d+)', function (req, res, next) {
-    res.render('../views/users/view');
-    res.send('user id ');
+router.get('/:id', function (req, res, next) {
+
+    async.parallel([
+        function (callback) {
+            request('GET', 'http://52.192.132.93:8080/searchUser?findByUserId=' + req.params.id)
+                .end(function (err, response) {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log(JSON.stringify(response.body));
+                    callback(err, response);
+                });
+        }
+    ], function (err, results) {
+        if (err) {
+            throw err;
+        }
+        console.log(JSON.stringify(results[0].body.data[0]));
+        res.render('../views/users/view', {
+            user: results[0].body.data[0]
+        });
+    });
+
+
+    //console.log('test');
+
+    //res.render('../views/users/view');
+    //res.send('user id ');
 });
 
 
